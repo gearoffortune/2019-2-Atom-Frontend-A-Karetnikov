51d0c20fa00092b2f1ee08e983822bcd93c55ec2
gearoffortune <hopelessuntildeath@gmail.com>
2019-11-26 19:51:50 +0300

    main screen finished

--- web-components/src/components/MainScreen.js
+++ web-components/src/components/MainScreen.js
@@ -1,14 +1,97 @@
+import avatarMe from '../assets/personavatar_me.png';
+import avatarAnn from '../assets/personavatar_ann.png';
+import avatarSasha from '../assets/personavatar_sasha.png';
+
 const template = document.createElement('template');
 template.innerHTML = `
     <main-header></main-header>
-`
+    <div class="all-chats"></div>
+    <new-message></new-message>
+    <style>
+      new-message {
+        position: fixed;
+        bottom: 50px;
+        right: 50px;
+      }
+    </style>
+`;
 
 class MainScreen extends HTMLElement {
-    constructor() {
-        super();
-        this._shadowRoot = this.attachShadow({ mode: 'open' });
-        this._shadowRoot.appendChild(template.content.cloneNode(true));
-    }
+  constructor() {
+    super();
+    this._shadowRoot = this.attachShadow({ mode: 'open' });
+    this._shadowRoot.appendChild(template.content.cloneNode(true));
+    this._chats = this._shadowRoot.querySelector('.all-chats');
+    this._loadLocalStorage();
+    this._loadChats();
+  }
+
+  _loadLocalStorage() {
+    this._chatData = JSON.parse(localStorage.getItem('chatdata')) || {
+      chats:
+        [{
+          type: 'oneByOne',
+          users: [{ userName: 'Me', avatar: avatarMe }],
+          unreadCount: 0,
+          messages:
+            [{
+              value: 'try',
+              user: 'Me',
+              date: 1572186026733,
+              isClientMessage: false,
+              isRead: false,
+              isRecieved: true,
+            }],
+        },
+        {
+          type: 'oneByOne',
+          users: [{ userName: 'Ann', avatar: avatarAnn }],
+          unreadCount: 1,
+          messages:
+          [{
+            value: 'Where the hell are you?',
+            user: 'Me',
+            date: 1574771940093,
+            isClientMessage: true,
+            isRead: true,
+            isRecieved: true,
+          }],
+        },
+        {
+          type: 'oneByOne',
+          users: [{ userName: 'Sasha', avatar: avatarSasha }],
+          unreadCount: 1,
+          messages:
+          [{
+            value: "What's your ETA?",
+            user: 'Me',
+            date: 1574777062635,
+            isClientMessage: true,
+            isRead: false,
+            isRecieved: true,
+          }],
+        }],
+    };
+  }
+
+  _saveLocalStorage() {
+    localStorage.setItem('chatdata', JSON.stringify(this._chatData));
+  }
+
+  _loadChats() {
+    this._chatData.chats.forEach((chat) => {
+      this._chats.innerHTML += `<single-chat 
+        chatname="${chat.users[0].userName}" 
+        chatavatar="${chat.users[0].avatar}" 
+        lastmessage="${chat.messages[chat.messages.length - 1].value}" 
+        senttime="${chat.messages[chat.messages.length - 1].date}"
+        isclientmessage="${chat.messages[chat.messages.length - 1].isClientMessage}"
+        isread="${chat.messages[chat.messages.length - 1].isRead}"
+        isrecieved="${chat.messages[chat.messages.length - 1].isRecieved}"
+        chatavatar=""
+        messagenumber="${chat.messages.filter((x) => x.isRecieved && !x.isRead).length}"></single-chat>`;
+    });
+  }
 }
 
 customElements.define('main-screen', MainScreen);
--- web-components/src/components/SingleChat.js
+++ web-components/src/components/SingleChat.js
@@ -1,10 +1,84 @@
+import oneTick from '../assets/oneTick.svg';
+import twoTicks from '../assets/twoTicks.svg';
 
+const template = document.createElement('template');
+template.innerHTML = `
+    <div class="single-chat">
+        <div class="single-chat__avatar">
+
+        </div>
+        <div class="single-chat__name-and-message">
+            <h1 class="chat-name"></h1>
+            <p class="message-text"></p>
+        </div>
+        <div class="single-chat__unread-and-time">
+            <p class="message-time"></p>
+            <div class="unread-count"></div>
+        </div>
+    </div>
+    <style>
+        .single-chat {
+            display: flex;
+            justify-content: space-between;
+            border-bottom: 1px solid #666666;
+        }
+        .single-chat__name-and-message {
+            flex-grow: 1;
+        }
+        .message-text {
+          color: #666666;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          height: 1em;
+        }
+        .unread-count > svg {
+          height: 48px;
+          fill: #8e24aa;
+        }
+        .single-chat__avatar {
+          flex-basis: 2em;
+        }
+        .unread-count__counter {
+          color: white;
+          background-color: #8e24aa;
+          text-align: center;
+          border-radius: 1em;
+        }
+    </style>
+`;
 
 
 class SingleChat extends HTMLElement {
-    constructor() {
-        super();
+  constructor() {
+    super();
+    this._shadowRoot = this.attachShadow({ mode: 'open' });
+    this._shadowRoot.appendChild(template.content.cloneNode(true));
+    this.chatName = this.getAttribute('chatname');
+    this.chatAvatar = this.getAttribute('chatavatar');
+    this.lastMessage = this.getAttribute('lastmessage');
+    this.sentTime = this.getAttribute('senttime');
+    this.isClientMessage = this.getAttribute('isclientmessage');
+    this.isRead = this.getAttribute('isread');
+    this.isRecieved = this.getAttribute('isrecieved');
+    this.messageNumber = this.getAttribute('messagenumber');
+
+    this._shadowRoot.querySelector('.chat-name').innerText = this.chatName;
+
+    this._shadowRoot.querySelector('.message-text').innerText = this.lastMessage;
+    this._shadowRoot.querySelector('.message-time').innerText = (new Date(parseInt(this.sentTime, 10))).toTimeString().slice(0, 5);
+
+    this._shadowRoot.querySelector('.single-chat__avatar').innerHTML = `<img src="${this.chatAvatar}">`;
+
+    if (this.isClientMessage === 'true') {
+      if (this.isRead === 'true') {
+        this._shadowRoot.querySelector('.unread-count').innerHTML = twoTicks;
+      } else if (this.isRecieved === 'true') {
+        this._shadowRoot.querySelector('.unread-count').innerHTML = oneTick;
+      }
+    } else {
+      this._shadowRoot.querySelector('.unread-count').innerHTML = `<p class="unread-count__counter">${this.messageNumber}</p>`;
     }
+  }
 }
 
 customElements.define('single-chat', SingleChat);
